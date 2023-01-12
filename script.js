c897a817cb95d4662798e6d7785c0af7bf3f9bf8
Emre1555 <emreyuksel4253@gmail.com>
2023-01-12 23:23:36 +0300

    commit

--- script.js
+++ script.js
@@ -1,46 +1,68 @@
 const Gameboard = (()=>{
-    const gameboard = ["o","x","o","x","o","x","o","x","o"]
+    const gameboard = [null,null,null,null,null,null,null,null,null]
+    const gameboardDom = document.querySelectorAll(".gameboard")[0]
 
-    return{gameboard}
+    return{gameboard, gameboardDom}
 })()
 
 const displayController = (() => {
-    const num0 = document.querySelectorAll(".num-0")[0]
-    const num1 = document.querySelectorAll(".num-1")[0]
-    const num2 = document.querySelectorAll(".num-2")[0]
-    const num3 = document.querySelectorAll(".num-3")[0]
-    const num4 = document.querySelectorAll(".num-4")[0]
-    const num5 = document.querySelectorAll(".num-5")[0]
-    const num6 = document.querySelectorAll(".num-6")[0]
-    const num7 = document.querySelectorAll(".num-7")[0]
-    const num8 = document.querySelectorAll(".num-8")[0]
-
-
-    num0.innerHTML = Gameboard.gameboard[0]
-    num1.innerHTML = Gameboard.gameboard[1]
-    num2.innerHTML = Gameboard.gameboard[2]
-    num3.innerHTML = Gameboard.gameboard[3]
-    num4.innerHTML = Gameboard.gameboard[4]
-    num5.innerHTML = Gameboard.gameboard[5]
-    num6.innerHTML = Gameboard.gameboard[6]
-    num7.innerHTML = Gameboard.gameboard[7]
-    num8.innerHTML = Gameboard.gameboard[8]
-
+    function display(){
+        document.querySelectorAll(".num-0")[0].innerHTML = Gameboard.gameboard[0]
+        document.querySelectorAll(".num-1")[0].innerHTML = Gameboard.gameboard[1]
+        document.querySelectorAll(".num-2")[0].innerHTML = Gameboard.gameboard[2]
+        document.querySelectorAll(".num-3")[0].innerHTML = Gameboard.gameboard[3]
+        document.querySelectorAll(".num-4")[0].innerHTML = Gameboard.gameboard[4]
+        document.querySelectorAll(".num-5")[0].innerHTML = Gameboard.gameboard[5]
+        document.querySelectorAll(".num-6")[0].innerHTML = Gameboard.gameboard[6]
+        document.querySelectorAll(".num-7")[0].innerHTML = Gameboard.gameboard[7]
+        document.querySelectorAll(".num-8")[0].innerHTML = Gameboard.gameboard[8]
+    }
+    display()
+    return{display}
 })()
 
 
-var Player = (function(name, mark){
+var Player = (name, mark) =>{
     this.name = name
-    this.mark = mark
+    this.mark = mark    
     
     function play(location){
-        if(Gameboard.game=""){
-            Gameboard.game[location] = mark 
-        } else {
-            console.log("cannot mark that")
+        Gameboard.gameboard[location] = mark 
+        displayController.display()
+    }   
+
+    return {name, mark, play}
+}
+
+const Game = (()=>{
+    let moves = 0
+    let turn = "player1"
+
+    function move(location){
+        if(Gameboard.gameboard[location]==null){
+        moves = moves +1
+        if(turn=="player1"){
+            player1.play(location)
+            turn = "player2"
+        }else if(turn == "player2" ){
+            player2.play(location)
+            turn = "player1"
         }
     }
-
+    }
+    return {move} 
+    
 })()
+
+Gameboard.gameboardDom.addEventListener("click", function(event){
+    if(event.target.id >= 0 && event.target.id < 9){
+        Game.move(event.target.id)
+    }
+})
+
+    const player1 = Player("player1", "x")
+    const player2 = Player("player2", "o") 
+
+
 
 
